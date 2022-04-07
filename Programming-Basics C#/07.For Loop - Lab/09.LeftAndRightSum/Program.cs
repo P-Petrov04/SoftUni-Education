35efd8e3b635fbd518cff1fe580e79a8f238dca3
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-07 14:12:19 +0300

    Added first exercises with for loop

---
+++ Programming-Basics C#/07.For Loop - Lab/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sumLeft += number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sumRight += number;
+            }
+
+            if (sumLeft == sumRight)
+            {
+                Console.WriteLine($"Yes, sum = {sumLeft}");
+            }
+            else
+            {
+                int sum = sumLeft - sumRight;
+                sum = Math.Abs(sum);
+                Console.WriteLine($"No, diff = {sum}");
+
+            }
+        }
+    }
+}
