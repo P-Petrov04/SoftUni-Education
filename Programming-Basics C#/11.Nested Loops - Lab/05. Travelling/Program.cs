eff195e0776141f352587469a6a383c2e1c38404
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-09 15:26:49 +0300

    Added exercises with Nested Loops

---
+++ Programming-Basics C#/11.Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while (destination != "End")
+            {
+                double currentMoney = 0;
+                double budjed = double.Parse(Console.ReadLine());
+                while (currentMoney < budjed)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    currentMoney += money;
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming-Basics C#/11.Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rooms; i++)
+            {
+                Console.Write($"L{floors}{i} ");
+                
+            }
+            Console.WriteLine();
+            if (floors > 1)
+            {
+                for (int i = floors - 1; i >= 1; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        for (int j = 0; j < rooms; j++)
+                        {
+                            Console.Write($"O{i}{j} ");  
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        for (int j = 0; j < rooms; j++)
+                        {
+                            Console.Write($"A{i}{j} ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+            
+        }
+    }
+}
