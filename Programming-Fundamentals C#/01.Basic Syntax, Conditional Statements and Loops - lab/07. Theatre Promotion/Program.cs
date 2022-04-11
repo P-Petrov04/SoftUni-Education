11465b09c6d217b9dbf85e8465b06e38ecb7db3e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-11 18:14:23 +0300

    Add solutions of other exercises - "Foreign Language", "Theatre Promotion", "Divisible by 3" and "Sum of Odd Numbers"

---
+++ Programming-Fundamentals C#/01.Basic Syntax, Conditional Statements and Loops - lab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int priceForTicket = 0;
+
+            if (typeOfDay == "Weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    priceForTicket = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    priceForTicket = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    priceForTicket = 12;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            else if (typeOfDay == "Weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    priceForTicket = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    priceForTicket = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    priceForTicket = 15;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            else
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    priceForTicket = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    priceForTicket = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    priceForTicket = 10;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+
+            if (priceForTicket != 0)
+            {
+            Console.WriteLine($"{priceForTicket}$");
+            }
+                    
+        }
+    }
+}
