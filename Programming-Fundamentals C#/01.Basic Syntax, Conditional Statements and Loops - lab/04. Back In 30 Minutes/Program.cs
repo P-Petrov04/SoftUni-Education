281afffd4c614b54b9af0ec954c16c4cf4c0df0b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-11 18:12:25 +0300

    Added next solutions of "Back in 30 Minutes" and "Month Printer"

---
+++ Programming-Fundamentals C#/01.Basic Syntax, Conditional Statements and Loops - lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int hoursToMin = hours * 60;
+            int allMin = hoursToMin + minutes + 30;
+
+            int hourAfter30Mins = allMin / 60;
+            int minAfter30Min = allMin % 60;
+
+            if (minAfter30Min > 59)
+            {
+                hourAfter30Mins++;
+            }
+            if (hourAfter30Mins > 23)
+            {
+                hourAfter30Mins = 0;
+            }
+
+            Console.WriteLine($"{hourAfter30Mins}:{minAfter30Min:d2}");
+        }
+    }
+}
