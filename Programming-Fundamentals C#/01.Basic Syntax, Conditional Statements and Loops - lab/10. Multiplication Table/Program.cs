aaa7117fcede34ba02233926d001a3863f912643
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-11 18:16:25 +0300

    Add solutions for my last exercises - "Multiplication Table", "Multiplication Table 2.0", "Even Numbers" and "Holidays Between Two Dates"

---
+++ Programming-Fundamentals C#/01.Basic Syntax, Conditional Statements and Loops - lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int times = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                times++;
+                int value = num * times;
+                Console.WriteLine($"{num} X {times} = {value}");
+            }
+        }
+    }
+}
