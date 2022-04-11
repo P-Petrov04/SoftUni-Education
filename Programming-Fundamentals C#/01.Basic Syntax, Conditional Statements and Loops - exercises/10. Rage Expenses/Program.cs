49844ce48207bc821cd48c46b9ea64d9ba58c616
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-11 18:06:37 +0300

    Added more solutions of other exercises

---
+++ Programming-Fundamentals C#/01.Basic Syntax, Conditional Statements and Loops - exercises/10. Rage Expenses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLostGames = int.Parse(Console.ReadLine());
+            double headset = double.Parse(Console.ReadLine());
+            double mouse = double.Parse(Console.ReadLine());
+            double keyboard = double.Parse(Console.ReadLine());
+            double display = double.Parse(Console.ReadLine());
+
+            int headsetTrashed = countOfLostGames / 2;
+            int mouseTrashed = countOfLostGames / 3;
+            int keyboardTrashed = countOfLostGames / 6;
+            int displayTrashed = countOfLostGames / 12;
+
+            double totalPrice = headsetTrashed * headset + mouseTrashed * mouse + keyboardTrashed * keyboard + displayTrashed * display;
+
+            Console.WriteLine($"Rage expenses: {totalPrice:f2} lv.");
+        }
+    }
+}
