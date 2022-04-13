d19a4197765612b88b886326ce4303bc2f8e9d3b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-13 15:31:42 +0300

    Add solution of "Messages" exercise

---
+++ Programming-Fundamentals C#/More Exercises/Basic Syntax, Conditional Statements and Loops - More Exercises/05. Messages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder message = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string digits = Console.ReadLine();
+                int countOfDigits = digits.Length;
+                char digit = digits[0];
+                int mainDigit = (int)Char.GetNumericValue(digit);
+                if(mainDigit == 0)
+                {
+                    message.Append(" ");
+                }
+                else
+                {
+                    int offSet = (mainDigit - 2) * 3;
+                    if (mainDigit == 8 || mainDigit == 9)
+                    {
+                        offSet++;
+                    }
+                    int letterIndex = offSet + countOfDigits - 1;
+                    char myLetter = (char)(97 + letterIndex);
+
+                    message.Append(myLetter);
+                }
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
