c3ba9e26a4e1e3b70e4b5243328171bb76311fc5
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-14 15:02:31 +0300

    Add Data Types and Variables and solutions of 2 problems - "Convert Meters to Kilometers" and "Pounds to Dollars"

---
+++ Programming-Fundamentals C#/03.Data Types and Variables - Lab/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal britishPounds = decimal.Parse(Console.ReadLine());
+            decimal usDollars = britishPounds * (decimal)1.31;
+            Console.WriteLine($"{usDollars:f3}");
+        }
+    }
+}
