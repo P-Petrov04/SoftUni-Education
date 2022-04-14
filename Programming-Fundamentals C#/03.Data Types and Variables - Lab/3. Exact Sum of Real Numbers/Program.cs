0f4b4de791c171622ea794c4d90e87744f33994a
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-14 15:04:01 +0300

    Add solutions of 2 problems - "Exact Sum of Real Numbers" and "Centuries to Minutes"

---
+++ Programming-Fundamentals C#/03.Data Types and Variables - Lab/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal countNumbers = decimal.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= countNumbers; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
