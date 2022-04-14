7acb36c5b1b21cc27116421494b4383d89cbb160
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-14 15:05:03 +0300

    Add solutions of 2 problems - "Special Number" and "Revers Chars"

---
+++ Programming-Fundamentals C#/03.Data Types and Variables - Lab/5. Special Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                int num = i;
+                while (num != 0)
+                {
+                    int digit = num % 10;
+                    sum += digit;
+                    num = num / 10;
+                }
+                
+                bool isSpecial = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isSpecial}");
+                sum = 0;
+            }
+        }
+    }
+}
