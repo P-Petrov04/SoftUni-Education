e0ce4e02eea52b715074cd47dcb650c01bb790cd
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-14 15:08:37 +0300

    Add solutions of last 2 problems - "Refactor Volume of Pyramid" and "Refactor Special Numbers"

---
+++ Programming-Fundamentals C#/03.Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= countNumber; i++)
+            {
+                int number = i;
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+                bool isSpecial = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isSpecial}");
+                sum = 0;
+            }
+
+        }
+    }
+}
