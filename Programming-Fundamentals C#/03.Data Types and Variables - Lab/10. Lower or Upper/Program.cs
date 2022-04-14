cdd048bdc39288f49aa7dca2da384775a35e5365
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-14 15:07:13 +0300

    Add solutions of 2 problems - "Lower to Upper" and "Chars to String"

---
+++ Programming-Fundamentals C#/03.Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            bool c = char.IsUpper(char1);
+            if (c == true)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
