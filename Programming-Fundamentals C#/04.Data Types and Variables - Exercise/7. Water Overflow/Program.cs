3af4bce8be08e07530b4ba3f42e009c48f98e148
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:17:16 +0300

    Add solutions of 2 exercises - "Triples of Latin Letters" and "Water Overflow"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/7. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesFollow = int.Parse(Console.ReadLine());
+
+            int sumWater = 0;
+
+            for (int i = 1; i <= linesFollow; i++)
+            {
+                int water = int.Parse(Console.ReadLine());
+                if (sumWater + water > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+                sumWater += water;
+            }
+
+            Console.WriteLine(sumWater);
+        }
+    }
+}
