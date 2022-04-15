38e45319bab42ce975fea95301288080088e4899
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:19:18 +0300

    Add solutions of 2 exercises - "Pokemon" and "Snowballs"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/10. Pokemon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10._Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int pokedCounter = 0;
+            int originalPokePower = pokePower;
+            while (distance <= pokePower)
+            {
+                pokePower = pokePower - distance;
+                pokedCounter++;
+                if (pokePower <= 0)
+                {
+                    break;
+                }
+                if (originalPokePower / pokePower == 2 && originalPokePower % pokePower == 0 && exhaustionFactor != 0)
+                {
+                    pokePower = pokePower / exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokedCounter);
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countBalls = int.Parse(Console.ReadLine());
+            BigInteger MaxSnowBallValue = 0;
+            int snowBallSnow = 0;
+            int snowBallTime = 0;
+            int snowBallQuality = 0;
+
+            int maxSnowBallSnow = 0;
+            int maxSnowBallTime = 0;
+            int maxSnowBallQuality = 0;
+
+            for (int i = 1; i <= countBalls; i++)
+            {
+                snowBallSnow = int.Parse(Console.ReadLine());
+                snowBallTime = int.Parse(Console.ReadLine());
+                snowBallQuality = int.Parse(Console.ReadLine());
+
+                int snowBallValue = snowBallSnow / snowBallTime;
+                BigInteger finalSnowBallValue = BigInteger.Pow(snowBallValue, snowBallQuality);
+                
+                if (finalSnowBallValue >= MaxSnowBallValue)
+                {
+                    MaxSnowBallValue = finalSnowBallValue;
+                    maxSnowBallSnow = snowBallSnow;
+                    maxSnowBallTime = snowBallTime;
+                    maxSnowBallQuality = snowBallQuality;
+                }
+            }
+            Console.WriteLine($"{maxSnowBallSnow} : {maxSnowBallTime} = {MaxSnowBallValue} ({maxSnowBallQuality})");
+
+
+        }
+    }
+}
