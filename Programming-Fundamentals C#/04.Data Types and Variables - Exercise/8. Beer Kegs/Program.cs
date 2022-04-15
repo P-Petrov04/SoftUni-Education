2a9fa28f665cd665de0c16c6908b8ea880a16727
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:18:20 +0300

    Add solutions of 2 exercises - "Beer Kegs" and "Spice Must Flow"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/8. Beer Kegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _8._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCans = int.Parse(Console.ReadLine());
+
+            float volume = 0;
+            float MaxValue = 0;
+            string bigestModel = "";
+
+            for (int i = 1; i <= numberOfCans; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                volume = (float)(Math.PI * Math.Pow(radius, 2) * height);
+                if (volume > MaxValue)
+                {
+                    MaxValue = volume;
+                    bigestModel = model;
+                }
+            }
+
+            Console.WriteLine(bigestModel);
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/9. Spice Must Flow/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _9._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+
+            int daysCounter = 0;
+            int spice = 0;
+            int spiceAfterWorkDay = 0;
+            int allSpice = 0;
+            while (startingYield >= 100)
+            {
+                daysCounter++;
+                spice += startingYield;
+                if (spice >= 26)
+                {
+                    spiceAfterWorkDay = spice - 26;
+                    allSpice += spiceAfterWorkDay;
+                }
+                startingYield -= 10;
+                spice = 0;
+            }
+            int spiceAfteMineIsOut = allSpice;
+            if (spiceAfterWorkDay >= 26)
+            {
+                spiceAfteMineIsOut -= 26;
+            }
+            Console.WriteLine(daysCounter);
+            Console.WriteLine(spiceAfteMineIsOut);
+
+            
+        }
+    }
+}
