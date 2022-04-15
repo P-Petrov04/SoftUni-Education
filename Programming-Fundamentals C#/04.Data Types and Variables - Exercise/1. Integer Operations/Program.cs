c2f48a7c758ddf1f4188faee9b9419a06ea452a4
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:13:40 +0300

    Add Exercise - Data Types and Variables and solutions of 1 exercise - "Integer Operations"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/1. Integer Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            long operation1 =(long)num1 + num2;
+            long operation2 = operation1 / num3;
+            long operation3 = operation2 * num4;
+
+            Console.WriteLine(operation3);
+        }
+    }
+}
