72d13b0e952c8e1ab76cf3d55bbc0b54f27769b8
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:15:02 +0300

    Add solutions of 2 exercises - "Sum Digits" and "Elevator"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/3. Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+            int persons = int.Parse(Console.ReadLine());
+
+            int courses = capacity / persons;
+            int peopleLeft = capacity % persons;
+            if (peopleLeft != 0)
+            {
+                courses++;
+            }
+            Console.WriteLine(courses);
+
+  
+
+          
+
+        }
+    }
+}
