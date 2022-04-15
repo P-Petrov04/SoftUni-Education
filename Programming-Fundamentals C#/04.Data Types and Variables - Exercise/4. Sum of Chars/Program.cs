68dfde031d5a06bc7ad1277f9894c57dc300a46b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-15 14:16:04 +0300

    Add solutions of 2 exercises - "Sum of Chars" and "Print Part of the ASCII Table"

---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/4. Sum of Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countSimbols = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= countSimbols; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                int numOFLetter = (int)letter;
+                sum += numOFLetter;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/04.Data Types and Variables - Exercise/5. Print Part of the ASCII Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._Print_Part_of_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstIndex = int.Parse(Console.ReadLine());
+            int lastIndex = int.Parse(Console.ReadLine());
+
+            for (int i = firstIndex; i <= lastIndex; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+        }
+    }
+}
