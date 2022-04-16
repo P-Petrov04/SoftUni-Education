47f7a2030b2b4b554862f35efc7e54cbc174d562
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-16 17:12:12 +0300

    Add solution of "Decrypting Message"

---
+++ Programming-Fundamentals C#/More Exercises/Data Types and Variables - More Exercises/05. Decrypting Message/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+            StringBuilder message = new StringBuilder();
+            for (int i = 0; i < lines; i++)
+            {
+                char ch = char.Parse(Console.ReadLine());
+                message.Append((char)(ch + key));
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
