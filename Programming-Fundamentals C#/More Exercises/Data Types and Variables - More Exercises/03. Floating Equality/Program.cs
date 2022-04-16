f3fb755851bf3fae9fdc00a57f4f417a813291c5
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-16 17:09:37 +0300

    Add solution of "Floating Equality"

---
+++ Programming-Fundamentals C#/More Exercises/Data Types and Variables - More Exercises/03. Floating Equality/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            bool isEqual = true;
+            double diff = Math.Abs(firstNum - secondNum);
+            if (diff >= 0.000001)
+            {
+                isEqual = false;
+            }
+            Console.WriteLine(isEqual);
+        }
+    }
+}
