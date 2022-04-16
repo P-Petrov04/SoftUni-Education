c6112c4f9f9cbf508314ac54a655b55ad31a0731
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-16 17:08:35 +0300

    Add solution of "From Left to Right"

---
+++ Programming-Fundamentals C#/More Exercises/Data Types and Variables - More Exercises/02. From Left to The Right/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._From_Left_to_The_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                long[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                long greater = numbers.OrderByDescending(x => x = x).First();
+                long sum = 0;
+
+                while (greater != 0)
+                {
+                    sum += Math.Abs(greater % 10);
+                    greater /= 10;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
