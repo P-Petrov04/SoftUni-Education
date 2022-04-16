fc613824208f7be253f4258dc145bea03f6fe8f4
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-16 17:13:01 +0300

    Add solution of "Balanced Brackets"

---
+++ Programming-Fundamentals C#/More Exercises/Data Types and Variables - More Exercises/06. Balanced Brackets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int counterForOpen = 0;
+            int counterForClose = 0;
+            for (int i = 1; i <= lines; i++)
+            {
+                string str = Console.ReadLine();
+                if (str == "(")
+                {
+                    counterForOpen++;
+                }
+                else if (str == ")")
+                {
+                    counterForClose++;
+                    if (counterForOpen - counterForClose != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            
+            if (counterForOpen == counterForClose)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
