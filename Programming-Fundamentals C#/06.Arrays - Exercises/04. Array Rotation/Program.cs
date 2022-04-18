3cac88fe26b9cf8eef28abfbd56a7bc69f53d988
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:05:55 +0300

    Add solutions of 2 exercises - "Array Rotation" and "Top Integers"

---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/04. Array Rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int countOfRotations = int.Parse(Console.ReadLine());
+
+            int[] numbersAfterRotations = numbers;
+
+            for (int index = 0; index < countOfRotations; index++)
+            {
+                int firstNum = numbersAfterRotations[0];
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    numbersAfterRotations[i] = numbers[i + 1];
+                }
+                numbersAfterRotations[numbers.Length - 1] = firstNum;
+            }
+
+            Console.WriteLine(string.Join(" ", numbersAfterRotations));
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/05. Top Integers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopInteger = true;
+                for (int j = i; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] <= numbers[j + 1])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+            
+
+            //var arr = Console.ReadLine()
+            //            .Split()
+            //            .Select(int.Parse)
+            //            .ToArray();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    var currentNumber = arr[i];
+            //    var isItBigger = true;
+            //    for (int k = i + 1; k < arr.Length; k++)
+            //    {
+            //        if (currentNumber <= arr[k])
+            //        {
+            //            isItBigger = false;
+            //        }
+            //    }
+            //    if (isItBigger)
+            //    {
+            //        Console.Write($"{arr[i]} ");
+            //    }
+            //}
+
+        }
+    }
+}
