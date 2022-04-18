f7ad2a78c6d1e981f611229ea0e93273e5fb0bcb
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:04:47 +0300

    Add solutions of 2 exercises - "Common Elements" and "Zig-Zag Arrays"

---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+
+            for (int indexArr2 = 0; indexArr2 <= arr2.Length - 1; indexArr2++)
+            {
+                for (int indexArr1 = 0; indexArr1 <= arr1.Length - 1; indexArr1++)
+                {
+                    if (arr2[indexArr2] == arr1[indexArr1])
+                    {
+                        Console.Write($"{arr2[indexArr2]} ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[countOfLines]; 
+            int[] arr2 = new int[countOfLines]; 
+
+            for (int line = 0; line < countOfLines; line++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if(line % 2 == 0)
+                {
+                    arr1[line] = numbers[0];
+                    arr2[line] = numbers[1];
+                }
+                else
+                {
+                    arr1[line] = numbers[1];
+                    arr2[line] = numbers[0];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
