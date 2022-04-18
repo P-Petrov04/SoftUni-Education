1417234bfef9c485bdb997963ecc92299393f19b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:07:03 +0300

    Add solutions of 2 exercises - "Equal Sum" and "Max Sequence of Equal Elements"

---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/06. Equal Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+                for (int k = i; k < numbers.Length - 1; k++)
+                {
+                    rightSum += numbers[k + 1];
+                }
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine($"{i}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int currentNum = -1;
+            int maxLength = -1;
+            int numOfSequence = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                currentNum = numbers[i];
+                int sequenceCounter = 1;
+
+                for (int j = i; j < numbers.Length - 1; j++)
+                {
+                    int nextNum = numbers[j + 1];
+
+                    if (currentNum != nextNum)
+                    {
+                        break;
+                    }
+                    sequenceCounter++;
+                }
+
+                int[] currentSequence = new int[sequenceCounter];
+
+                if (maxLength < currentSequence.Length)
+                {
+                    maxLength = currentSequence.Length;
+                    numOfSequence = currentNum;
+                }
+            }
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                Console.Write($"{numOfSequence} ");
+            }
+        }
+    }
+}
