c9409fbadb1389d481e422a0773d6d2c3a8750cb
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:03:41 +0300

    Add .sln file of Arrays - Exercise and solution of "Train"

---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countWagons = int.Parse(Console.ReadLine());
+            int[] passengers = new int[countWagons];
+
+            int allPassengers = 0;
+
+            for (int i = 0; i <= passengers.Length - 1; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                allPassengers += passengers[i];
+            }
+
+            Console.WriteLine(string.Join(' ', passengers));
+            Console.WriteLine(allPassengers);
+        }
+    }
+}
