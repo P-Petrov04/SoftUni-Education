f6f02170ae147701714681f5c445133e9987289d
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:02:09 +0300

    Add solutions of last 2 exercises - "Equal Arrays" and "Condense Array to Number"

---
+++ Programming-Fundamentals C#/05.Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstLength = numbers.Length;
+
+            for (int i = 0; i < firstLength - 1; i++)
+            {
+                int[] condensed = new int[numbers.Length - 1];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    condensed[j] += numbers[j] + numbers[j + 1];
+                }
+                numbers = condensed;
+            }
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
