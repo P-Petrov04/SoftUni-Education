b0e004d177776ad0f9c303ec6086d72a53117ca9
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 15:58:43 +0300

    Add solutions of 2 exercises - "Day of Week" and "Print Numbers in Reverse Order"

---
+++ Programming-Fundamentals C#/05.Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbersArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbersArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = numbersArray.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbersArray[i]} ");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
