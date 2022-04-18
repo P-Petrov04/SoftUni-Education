d3ec08360d09e946e3d2be6ea81088058741237f
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 15:59:55 +0300

    Add solutions of 2 exercises - "Rounding Numbers" and "Reverse Array of Strings"

---
+++ Programming-Fundamentals C#/05.Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double[] parsedInput = new double[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                parsedInput[i] = double.Parse(input[i]);
+            }
+
+            for (int i = 0; i < parsedInput.Length; i++)
+            {
+                Console.WriteLine($"{parsedInput[i]} => {Math.Round(parsedInput[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
