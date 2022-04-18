c531532852d0c5d50b8eab7091bf154de6fcf419
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-18 16:07:56 +0300

    Add solutions of 2 exercises - "Magic Sum" and "Kamino Factory"

---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/08. Magic Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumToFind = int.Parse(Console.ReadLine());
+
+            int[] numForSum = new int[2];
+            int currentSum = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length - 1; j++)
+                {
+                    currentSum = 0;
+                    currentSum = numbers[i] + numbers[j + 1];
+                    if (currentSum == sumToFind)
+                    {
+                        numForSum[0] = numbers[i];
+                        numForSum[1] = numbers[j + 1];
+                        Console.Write(string.Join(" ", numForSum));
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/06.Arrays - Exercises/09. Kamino Factory/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthSequence = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine().Split("!");
+
+            int counterOfSequence = 1;
+            int maxCount = -1;
+            int bestCounterSequence = 0;
+            int bestSum = 0;
+            int[] bestSequence = new int[lengthSequence];
+            int sum = 0;
+
+            while (input[0] != "Clone them")
+            {
+                int[] currentSequence = new int[input.Length];
+                
+                for (int i = 0; i < lengthSequence; i++)
+                {
+                    currentSequence[i] = int.Parse(input[i]);
+                }
+
+                for (int i = 0; i < currentSequence.Length; i++)
+                {
+                    int currentNum = currentSequence[i];
+                    for (int j = i; j < currentSequence.Length - 1; j++)
+                    {
+                        int nextNum = currentSequence[j + 1];
+
+                        if (currentNum != nextNum)
+                        {
+                            break;
+                        }
+                        if (currentNum == 1)
+                        {
+                        counterOfSequence++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    
+                    if (counterOfSequence >= maxCount)
+                    {
+                        maxCount = counterOfSequence;
+                        bestSequence = currentSequence;
+                    }
+
+                    if (currentNum == 1)
+                    {
+                        sum += currentNum;
+                    }
+                    
+                    if (sum > bestSum)
+                    {
+                        bestSum = sum;
+                    }
+                    
+                    if (counterOfSequence > bestCounterSequence)
+                    {
+                        bestCounterSequence = counterOfSequence;
+                    }
+                }
+
+                input = Console.ReadLine().Split("!");
+            }
+
+            Console.WriteLine($"Best DNA sample {bestCounterSequence} with sum: {bestSum}.");
+            Console.WriteLine(string.Join(" ", bestSequence));
+            
+        }
+    }
+}
