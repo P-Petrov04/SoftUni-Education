2d0a8ed39433a671a4b5a8ac6be1f0e5ca5434a8
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-20 19:24:34 +0300

    Add solutions of 2 exercises - "Math Power" and "Greater of Two Values"

---
+++ Programming-Fundamentals C#/07.Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int powerNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PoweredNum(number, powerNum));
+        }
+
+        static double PoweredNum(double @base, int power)
+        {
+            double poweredNum = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                poweredNum *= @base;
+            }
+
+            return poweredNum;
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/07.Methods - Lab/09. Greater of Two Values/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _09._Greater_of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valueType = Console.ReadLine();
+
+            if (valueType == "int")
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(num1 , num2));
+            }
+            else if (valueType == "char")
+            {
+                char chr1 = char.Parse(Console.ReadLine());
+                char chr2 = char.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(chr1, chr2));
+            }
+            else if (valueType == "string")
+            {
+                string text1 = Console.ReadLine();
+                string text2 = Console.ReadLine();
+                Console.WriteLine(GetMax(text1, text2));
+            } 
+            
+
+        }
+
+        static int GetMax(int first, int second)
+        {
+            return first >= second ? first : second;
+        }
+        static char GetMax(char first, char second)
+        {
+            return first >= second ? first : second;
+        }
+        static string GetMax(string first, string second)
+        {
+            return first.CompareTo(second) >= 0 ? first : second;
+        }
+    }
+}
