24dc54caa7796c01341de87fb0a1651588913c5b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-20 19:21:48 +0300

    Add solutions of 2 exercises - "Grades" and "Calculations"

---
+++ Programming-Fundamentals C#/07.Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string methodForCalculation = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            Calculating(methodForCalculation, number1, number2);
+        }
+
+        static void Calculating(string method, int firstNum, int secondNum)
+        {
+            if (method == "add")
+            {
+                Console.WriteLine($"{firstNum + secondNum}");
+            }
+            else if (method == "multiply")
+            {
+                Console.WriteLine($"{firstNum * secondNum}");
+            }
+            else if (method == "subtract")
+            {
+                Console.WriteLine($"{firstNum - secondNum}");
+            }
+            else if (method == "divide")
+            {
+                Console.WriteLine($"{firstNum / secondNum}");
+            }
+        }
+    }
+}
