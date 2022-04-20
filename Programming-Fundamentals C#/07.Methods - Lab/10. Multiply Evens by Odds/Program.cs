34c770f00c517cb521e7e2f493ce6596b1d18064
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-20 19:25:30 +0300

    Add solutions of 2 exercises - "Multiply Evens by Odds" and "Math operations"

---
+++ Programming-Fundamentals C#/07.Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            
+            Console.WriteLine(SumEvenNumbers(number) * SumOddNumbers(number));
+        }
+
+        static int SumEvenNumbers(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+
+                number = number / 10;
+            }
+
+            return sum;
+        }
+
+       static int SumOddNumbers(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+
+                number = number / 10;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/07.Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            char operationsSign = char.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Operating(number1, operationsSign, number2));
+        }
+
+        static int Operating(int num1, int operation, int num2)
+        {
+            int value = 0;
+            if (operation == '/')
+            {
+                value = num1 / num2;
+            }
+            else if (operation == '-')
+            {
+                value = num1 - num2;
+            }
+            else if (operation == '+')
+            {
+                value = num1 + num2;
+            }
+            else if (operation == '*')
+            {
+                value = num1 * num2;
+            }
+
+            return value;
+        }
+    }
+}
