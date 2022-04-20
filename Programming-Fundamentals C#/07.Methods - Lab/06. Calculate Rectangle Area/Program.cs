9698f588fdbe5c359ece5d3944ccabaa4ce90ed4
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-20 19:23:36 +0300

    Add solutions of 2 exercises - "Calculate Rectangle Area" and "Repeat String"

---
+++ Programming-Fundamentals C#/07.Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculatingArea(side, height));
+        }
+
+        static int CalculatingArea(int side, int height)
+        {
+            return side * height;
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/07.Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        private static object stringBuilder;
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int countRepeats = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ReapeaingString(text, countRepeats));
+        }
+
+        static string ReapeaingString(string text, int repeat) //fast way
+        {
+            StringBuilder repeatedString = new StringBuilder();
+
+            for (int i = 0; i < repeat; i++)
+            {
+                repeatedString.Append(text);
+            }
+
+            return repeatedString.ToString();
+        }
+
+        //static string RepeatStringSlow(string inputString, int repeatNumber) //slow way
+        //{
+        //    string result = string.Empty;
+        //    for (int i = 0; i < repeatNumber; i++)
+        //    {
+        //        result += inputString;
+        //    }
+
+        //    return result;
+        //}
+    }
+}
