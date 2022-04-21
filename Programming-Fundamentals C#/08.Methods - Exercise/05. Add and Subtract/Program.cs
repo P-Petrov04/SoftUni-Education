af0b40851f09a351e85466e7f663381ece1ff8a3
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-21 14:56:32 +0300

    Add solutions of 2 exercises - "Add and Subtract" and "Middle Characters"

---
+++ Programming-Fundamentals C#/08.Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = SumOfNum1AndNum2(num1, num2);
+
+            int finalResult = SubtractNum3FromSum(sum, num3);
+            Console.WriteLine(finalResult);
+        }
+        static int SumOfNum1AndNum2 (int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        static int SubtractNum3FromSum (int sum, int num3)
+        {
+            int subtract = sum - num3;
+            return subtract;
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/08.Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length % 2 != 0)
+            {
+                char middleCh = LenghtIsOdd(input);
+                Console.WriteLine(middleCh);
+            }
+            else
+            {
+                char[] middleCh = LenghtIsEven(input);
+                Console.WriteLine(string.Join("", middleCh));
+            }
+        }
+
+        static char LenghtIsOdd (string input)
+        {
+            char middleCh = '.';
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char currentCh = input[i];
+                    int charsBeforeCurrentChar = 0;
+                    int charsAfterCurrentChar = 0;
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        charsBeforeCurrentChar++;
+                    }
+                    for (int j = i + 1; j < input.Length; j++)
+                    {
+                        charsAfterCurrentChar++;
+                    }
+
+                    if (charsBeforeCurrentChar == charsAfterCurrentChar)
+                    {
+                        middleCh = currentCh;
+                        break;
+                    }
+                }
+            
+            return middleCh;
+        }
+
+        static char[] LenghtIsEven (string input)
+        {
+            char[] middleChArray = new char[2];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentCh = input[i];
+                int charsBeforeCurrentChar = 0;
+                int charsAfterCurrentChar = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    charsBeforeCurrentChar++;
+                }
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    charsAfterCurrentChar++;
+                }
+
+                if (charsBeforeCurrentChar - charsAfterCurrentChar == -1)
+                {
+                    middleChArray[0] = input[i];
+                    middleChArray[1] = input[i + 1];
+                }
+            }
+            return middleChArray;
+        }
+    }
+}
