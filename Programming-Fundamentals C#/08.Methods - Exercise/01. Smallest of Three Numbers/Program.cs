ddac6e78b2726e68c6761c65ac9e97d003aaf83e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-21 14:54:13 +0300

    Add solutions of 2 exercises - "Smallest of Three Numbers" and "Vowels Count"

---
+++ Programming-Fundamentals C#/08.Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            PrintSmallestNum(number1, number2, number3);
+        }
+
+        static void PrintSmallestNum(int num1,int num2,int num3)
+        {
+            if (num1 <= num2 & num1 <= num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 <= num1 & num2 <= num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3 <= num1 & num3 <= num2)
+            {
+                Console.WriteLine(num3);
+            }
+
+            
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/08.Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            text = text.ToLower();
+            Console.WriteLine(CountVowels(text));
+        }
+
+        static int CountVowels(string text)
+        {
+            int vowelsCounter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentLabel = text[i];
+
+                if (currentLabel == 'a' || currentLabel == 'e' || currentLabel == 'i' || currentLabel == 'o' || currentLabel == 'u' || currentLabel == 'y')
+                {
+                    vowelsCounter++;
+                }
+            }
+
+            return vowelsCounter;
+        }
+    }
+}
