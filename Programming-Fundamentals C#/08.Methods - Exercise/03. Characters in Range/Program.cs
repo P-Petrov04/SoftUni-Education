eed345b84247190ab9cdf2510a259c8f5b9d4e38
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-21 14:55:24 +0300

    Add solutions of 2 exercises - "Characters in Range" and "Password Validator"

---
+++ Programming-Fundamentals C#/08.Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char chStart = char.Parse(Console.ReadLine());
+            char chEnd = char.Parse(Console.ReadLine());
+
+            char[] charArray = new char[2] { chStart, chEnd };
+            SwapChStartAndCharEnd(charArray, chStart, chEnd);
+            PrintCharsFromStartToEnd(charArray, chStart, chEnd);
+            
+        }
+
+        static char[] SwapChStartAndCharEnd (char[] chArray,char ch1, char ch2)
+        {
+            int valueCh1 = ch1;
+            int valueCh2 = ch2;
+
+            if (valueCh1 > valueCh2)
+            {
+                chArray[0] = ch2;
+                chArray[1] = ch1;
+            }
+
+            return chArray;
+        }
+
+        static void PrintCharsFromStartToEnd (char[] chArray, char ch1, char ch2)
+        {
+            for (int i = chArray[0] + 1; i < chArray[1]; i++)
+            {
+                char currentCh = (char)i;
+                Console.Write($"{currentCh} ");
+            }
+        }
+       
+    }
+}
---
+++ Programming-Fundamentals C#/08.Methods - Exercise/04. Password Validator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _04._Password_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            bool isValid = PrintCorresponding(password);
+
+            if (isValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        static bool PrintCorresponding (string password)
+        {
+            bool isValid = true;
+                if (!CheckPasswordLenght(password, isValid))
+                {
+                    Console.WriteLine("Password must be between 6 and 10 characters");
+                    isValid = false;
+                }
+                if (!CheckContainsOnlyLetterAndDigits(password, isValid))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                    isValid = false;
+                }
+                if (!CheckCountOfDigits(password, isValid))
+                {
+                    Console.WriteLine("Password must have at least 2 digits");
+                    isValid = false;
+                }
+
+            return isValid;
+        }
+        static bool CheckPasswordLenght (string passwrod, bool isValid)
+        {
+            if (passwrod.Length >= 6 && passwrod.Length <= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool CheckContainsOnlyLetterAndDigits (string password, bool isValid)
+        {
+            foreach (char ch in password)
+            {
+                if (!Char.IsLetterOrDigit(ch))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        static bool CheckCountOfDigits (string password, bool isValid)
+        {
+            int digitsCount = 0;
+            foreach (char ch in password)
+            {
+                if (Char.IsDigit(ch))
+                {
+                    digitsCount++;
+                }
+            }
+
+            return digitsCount >= 2;
+        }
+    }
+}
