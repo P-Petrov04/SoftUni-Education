59a41290697d28a00ec212584a891128b0ffdddc
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-21 14:57:35 +0300

    Add solutions of 2 exercises - "NxN Matrix" and "Factorial Division"

---
+++ Programming-Fundamentals C#/08.Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+
+            PrintMatrix(lenght);
+        }
+
+        static void PrintMatrix (int lenght)
+        {
+            for (int i = 1; i <= lenght; i++)
+            {
+                for (int j = 1; j <= lenght; j++)
+                {
+                    Console.Write(lenght + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/08.Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            double factOne = FactorialOfFirstNum(number1);
+            double factTwo = FactorialOfSecondNum(number2);
+            double finalValue = DevideFactorial1ToFactorial2(factOne, factTwo);
+
+            Console.WriteLine($"{finalValue:f2}");
+        }
+
+        static double DevideFactorial1ToFactorial2(double factorial1, double factorial2)
+        {
+            double devide = factorial1 / factorial2;
+            return devide;
+        }
+
+        static double FactorialOfFirstNum(int num1)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= num1; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+        }
+
+        static double FactorialOfSecondNum(int num2)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= num2; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
+
