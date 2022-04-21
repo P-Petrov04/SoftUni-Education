9a818350d0e08da6cac69d16a3ed7e5fa6eb05ee
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-21 14:58:38 +0300

    Add solutions of 2 exercises - "Palindrome Integers" and "Top Number"

---
+++ Programming-Fundamentals C#/08.Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input=Console.ReadLine()) != "END")
+            {
+                string reversedInput = ReverseString(input);
+
+                if (reversedInput == input)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        static string ReverseString (string input)
+        {
+            string reversedInput = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char currentCh = input[i];
+                reversedInput += currentCh;
+            }
+
+            return reversedInput;
+        }
+    }
+}
---
+++ Programming-Fundamentals C#/08.Methods - Exercise/10. Top Number/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int currentNum = 1; currentNum <= number; currentNum++)
+            {
+                bool isTopNumber = CheckNumber(currentNum);
+
+                if (isTopNumber == true)
+                {
+                    Console.WriteLine(currentNum);
+                }
+            }
+        }
+
+        static bool CheckNumber (int currentNum)
+        {
+            if (SumOfDigitsDivisibleBy8(currentNum))
+            {
+                if (CheckForOddDigit(currentNum))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool SumOfDigitsDivisibleBy8 (int currentNum)
+        {
+            int sum = 0;
+            while (currentNum != 0)
+            {
+                int currentDigit = currentNum % 10;
+                sum += currentDigit;
+                currentNum /= 10;
+            }
+
+            if (sum % 8 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool CheckForOddDigit (int currentNum)
+        {
+            int oddDigitsCount = 0;
+            while (currentNum != 0)
+            {
+                int currentDigit = currentNum % 10;
+                if (currentDigit % 2 != 0)
+                {
+                    oddDigitsCount++;
+                }
+                currentNum /= 10;
+            }
+
+            if (oddDigitsCount >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
