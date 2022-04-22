e421102e4238bfffa5dac4cf6797744ce0e8b804
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-22 18:05:15 +0300

    Add solution of "Data Types"

---
+++ Programming-Fundamentals C#/More Exercises/Methods - More Exercises/01. Data Types/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+
+            if (input == "int")
+            {
+                int result = int.Parse(secondInput) * 2;
+                Console.WriteLine(result);
+            }
+            else if (input == "real")
+            {
+                double result = double.Parse(secondInput) * 1.5;
+                Console.WriteLine($"{result:f2}");
+            }
+            else if (input == "string")
+            {
+                Console.WriteLine($"${secondInput}$");
+            }
+
+        }
+    }
+}
