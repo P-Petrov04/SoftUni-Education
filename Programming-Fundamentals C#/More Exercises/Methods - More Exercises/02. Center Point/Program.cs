36bf59af51421797479cad825819a4ff3eeaf3e3
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-22 18:06:01 +0300

    Add solution of "Center Point"

---
+++ Programming-Fundamentals C#/More Exercises/Methods - More Exercises/02. Center Point/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            
+            double sum = Math.Abs(x1) + Math.Abs(y1);
+            double secondSum = Math.Abs(x2) + Math.Abs(y2);
+
+            if (sum < secondSum)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}
