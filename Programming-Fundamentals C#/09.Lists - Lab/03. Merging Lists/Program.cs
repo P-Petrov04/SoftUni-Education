ef46973681fa87e84a69a13d6fd1f2715b291e06
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-24 20:56:03 +0300

    Add solution of "Merging Lists"

---
+++ Programming-Fundamentals C#/09.Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            int longerList = Math.Max(numbers1.Count, numbers2.Count);
+
+            for (int i = 0; i < longerList; i++)
+            {
+                if (i < numbers1.Count)
+                {
+                    result.Add(numbers1[i]);
+                }
+
+                if (i < numbers2.Count)
+                {
+                    result.Add(numbers2[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
