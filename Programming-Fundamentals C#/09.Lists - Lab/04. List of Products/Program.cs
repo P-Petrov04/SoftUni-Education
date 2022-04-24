27c694042abb08bee50d43bbb5fbd82d9b28289e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-24 20:56:37 +0300

    Add solution of "List of Products"

---
+++ Programming-Fundamentals C#/09.Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfProducts = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 1; i <= countOfProducts; i++)
+            {
+                string typeProduct = Console.ReadLine();
+                products.Add(typeProduct);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
