5192a9790c3faea2c4c63a7d5cbc695d53c7433f
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-25 20:37:28 +0300

    Add solution of "Change List"

---
+++ Programming-Fundamentals C#/10.Lists - Exercise/02. Change List/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                string[] commands = input.Split();
+                string typeOfCommand = commands[0];
+                if (typeOfCommand == "Delete")
+                {
+                    int element = int.Parse(commands[1]);
+                    numbers.RemoveAll(x => x == element);
+                }
+                else
+                {
+                    int element = int.Parse(commands[1]);
+                    int index = int.Parse(commands[2]);
+                    numbers.Insert(index, element);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
