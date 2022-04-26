ae9fe1187c82dab6307ebb82967c4a2fbbf8fa08
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-26 15:07:04 +0300

    Add solution of "Drum Set"

---
+++ Programming-Fundamentals C#/More Exercises/Lists - More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            List<int> drums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> originalDrums = new List<int>();
+            foreach (int drum in drums)
+            {
+                originalDrums.Add(drum);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "Hit it again, Gabsy!")
+            {
+                int power = int.Parse(command);
+                for (int i = 0; i < drums.Count; i++)
+                {
+                    drums[i] -= power;
+                    if (drums[i] <= 0)
+                    {
+                        if (money - originalDrums[i] * 3 <= 0)
+                        {
+                            drums.RemoveAt(i);
+                            originalDrums.RemoveAt(i);
+                            i--;
+                        }
+                        else
+                        {
+                            money -= originalDrums[i] * 3;
+                            drums[i] = originalDrums[i];
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", drums));
+            Console.WriteLine($"Gabsy has {money:f2}lv.");
+        }
+    }
+}
