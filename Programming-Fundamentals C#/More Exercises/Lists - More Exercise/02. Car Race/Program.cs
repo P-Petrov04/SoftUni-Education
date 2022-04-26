e23230a51719e563498828669a3842da62b57537
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-26 15:05:25 +0300

    Add solution of "Car Race"

---
+++ Programming-Fundamentals C#/More Exercises/Lists - More Exercise/02. Car Race/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> times = Console.ReadLine().Split().Select(int.Parse).ToList();
+            double sumLeft = 0;
+            double sumRight = 0;
+            int finishIndex = times.Count / 2 + 1;
+
+            for (int i = 1; i < finishIndex; i++)
+            {
+                int currentTime = times[i - 1];
+                if (currentTime != 0)
+                {
+                    sumLeft += currentTime;
+                }
+                else
+                {
+                    sumLeft *= 0.8;
+                }
+            }
+            for (int i = times.Count - 1; i >= finishIndex; i--)
+            {
+                int currentTime = times[i];
+                if (currentTime != 0)
+                {
+                    sumRight += currentTime;
+                }
+                else
+                {
+                    sumRight *= 0.8;
+                }
+            }
+
+            if (sumLeft < sumRight)
+            {
+                Console.WriteLine($"The winner is left with total time: {sumLeft}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {sumRight}");
+            }
+        }
+    }
+}
