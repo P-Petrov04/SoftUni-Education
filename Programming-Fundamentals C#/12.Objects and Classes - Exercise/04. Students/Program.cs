7211be3485a8fe6dd547fafc387f2f10e93c4809
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-28 19:05:36 +0300

    Add solution of "Students"

---
+++ Programming-Fundamentals C#/12.Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> allStudents = new List<Student>();
+            int countStudents = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countStudents; i++)
+            {
+                string[] studentArgs = Console.ReadLine().Split();
+                string firstName = studentArgs[0];
+                string lastName = studentArgs[1];
+                double grade = double.Parse(studentArgs[2]);
+
+                Student newStudent = new Student(firstName, lastName, grade);
+                allStudents.Add(newStudent);
+            }
+
+            List<Student> orderedStudents = allStudents.OrderByDescending(s => s.Grade).ToList();
+            foreach (Student student in orderedStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
