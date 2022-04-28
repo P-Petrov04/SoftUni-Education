1a7fad735ef9d160fefd0ff43fff128c1c296603
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-28 19:04:18 +0300

    Add solution of "Advertisement Message"

---
+++ Programming-Fundamentals C#/12.Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phrases = new List<string>()
+            {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product."
+            };
+            List<string> events = new List<string>()
+            {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+            List<string> authors = new List<string>()
+            {
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"
+            };
+            List<string> cities = new List<string>()
+            {
+                "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse"
+            };
+
+            int numberOfMessages = int.Parse(Console.ReadLine());
+            Random random = new Random();
+
+            for (int i = 1; i <= numberOfMessages; i++)
+            {
+                int randomIndexOfPhrases = random.Next(0, phrases.Count);
+                int randomIndexOfEvents = random.Next(0, events.Count);
+                int randomIndexOfAuthors = random.Next(0, authors.Count);
+                int randomIndexOfCities = random.Next(0, cities.Count);
+
+                Console.WriteLine($"{phrases[randomIndexOfPhrases]} " +
+                    $"{events[randomIndexOfEvents]} " +
+                    $"{authors[randomIndexOfAuthors]} – " +
+                    $"{cities[randomIndexOfCities]}.");
+            }
+        }
+    }
+}
