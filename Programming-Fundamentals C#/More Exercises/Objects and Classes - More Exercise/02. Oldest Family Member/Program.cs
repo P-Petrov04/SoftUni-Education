fdff612c8e267e91d3545a0741d719ee6f9c77f3
P-Petrov04 <petar.004.petrov@gmail.com>
2022-04-30 19:56:03 +0300

    Add solution of "Oldest Family Member"

---
+++ Programming-Fundamentals C#/More Exercises/Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Oldest_Family_Member
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Family
+    {
+        public Family()
+        {
+            this.AllPersons = new List<Person>();
+        }
+        public List<Person> AllPersons { get; set; }
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] personArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personArgs[0];
+                int age = int.Parse(personArgs[1]);
+
+                Person newMember = new Person(name, age);
+                family.AllPersons.Add(newMember);
+            }
+
+                Person oldestPerson = family.AllPersons.OrderByDescending(p => p.Age).FirstOrDefault();
+                Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
