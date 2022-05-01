e683e4e306ebcd8ca9b778519c3487ac00abc0c7
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-01 15:54:43 +0300

    Add solution of "Odd Occurrences"

---
+++ Programming-Fundamentals C#/13.Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            
+            Dictionary<string, int> wordsOccurences = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string currentWord = word.ToLower();
+                if (wordsOccurences.ContainsKey(currentWord))
+                {
+                    wordsOccurences[currentWord] += 1;
+                }
+                else
+                {
+                    wordsOccurences.Add(currentWord, 1);
+                }
+            }
+
+            foreach (var word in wordsOccurences)
+            {
+                if (word.Value % 2 == 0)
+                {
+                    wordsOccurences.Remove(word.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wordsOccurences.Keys));
+        }
+    }
+}
