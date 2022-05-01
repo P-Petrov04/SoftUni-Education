0a6dd8890db4ba44c08d49d22d235f43566abb73
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-01 15:55:10 +0300

    Add solution of "Word Synonyms"

---
+++ Programming-Fundamentals C#/13.Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countWords = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> wordsAndSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 1; i <= countWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (wordsAndSynonyms.ContainsKey(word))
+                {
+                    wordsAndSynonyms[word].Add(synonym);
+                }
+                else
+                {
+                    wordsAndSynonyms.Add(word, new List<string>());
+                    wordsAndSynonyms[word].Add(synonym);
+                }
+            }
+
+            foreach (var word in wordsAndSynonyms)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
