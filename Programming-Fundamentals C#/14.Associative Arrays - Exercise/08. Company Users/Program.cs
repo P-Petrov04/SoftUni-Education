611604a04c94cab09bce9d4333caa03437a2b265
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-02 21:17:02 +0300

    Add solution of "Company Users"

---
+++ Programming-Fundamentals C#/14.Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+            while (input != "End")
+            {
+                string[] arguments = input.Split(" -> ");
+
+                string companyName = arguments[0];
+                string id = arguments[1];
+
+                if (companyUsers.ContainsKey(companyName))
+                {
+                    if (companyUsers[companyName].Contains(id))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    companyUsers[companyName].Add(id);
+                }
+                else
+                {
+                    companyUsers.Add(companyName, new List<string>() { id });
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in companyUsers)
+            {
+                Console.WriteLine($"{item.Key}");
+                string[] users = item.Value.ToArray();
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"-- {user}");
+                }
+            }
+        }
+    }
+}
