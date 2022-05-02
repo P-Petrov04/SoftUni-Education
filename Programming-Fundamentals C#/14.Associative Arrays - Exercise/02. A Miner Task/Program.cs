97462ed910be39f3d053ff7f9eb4ec3711bf99b9
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-02 21:13:30 +0300

    Add solution of "A Miner Task"

---
+++ Programming-Fundamentals C#/14.Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int countOfReadedLines = 0;
+            int quantity = 0;
+            string resource = string.Empty;
+
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (input != "stop")
+            {
+                countOfReadedLines++;
+                
+                if (countOfReadedLines % 2 != 0)
+                {
+                    resource = input;
+                    if (!resources.ContainsKey(resource))
+                    {
+                        resources.Add(resource, 0);
+                    }
+                }
+                else
+                {
+                    quantity = int.Parse(input);
+                    resources[resource] += quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
