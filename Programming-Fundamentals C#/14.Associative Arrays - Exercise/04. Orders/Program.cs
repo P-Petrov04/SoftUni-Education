a899985e8d31a6ee86d0a0214cd8ed4cb87f2979
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-02 21:14:25 +0300

    Add solution of "Orders"

---
+++ Programming-Fundamentals C#/14.Associative Arrays - Exercise/04. Orders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<decimal>> product = new Dictionary<string, List<decimal>>();
+
+            while (input != "buy")
+            {
+                string[] productArg = input.Split();
+
+                string productName = productArg[0];
+                decimal price = decimal.Parse(productArg[1]);
+                int quantity = int.Parse(productArg[2]);
+
+                if (!product.ContainsKey(productName))
+                {
+                    product.Add(productName, new List<decimal>() {price, quantity});
+                }
+                else
+                {
+                    product[productName][0] = price;
+                    product[productName][1] += quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in product)
+            {
+                Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):f2}");
+            }
+        }
+    }
+}
