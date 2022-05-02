2223050da547c904692c0a5953295c3b273d7367
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-02 21:16:38 +0300

    Add solution of "Student Academy"

---
+++ Programming-Fundamentals C#/14.Associative Arrays - Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairsOfRows = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsArgs = new Dictionary<string, List<double>>();
+
+            for (int i = 1; i <= pairsOfRows; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (studentsArgs.ContainsKey(studentName))
+                {
+                    studentsArgs[studentName].Add(grade);
+                }
+                else
+                {
+                    studentsArgs.Add(studentName, new List<double>(){ grade });
+                }
+            }
+            Dictionary<string, double> secondDictionary = new Dictionary<string, double>();
+
+            foreach (var item in studentsArgs)
+            {
+                secondDictionary.Add(item.Key, item.Value.Average());
+            }
+
+            foreach (var item in secondDictionary)
+            {
+                if (!(item.Value >= 4.50))
+                {
+                    secondDictionary.Remove(item.Key);
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+                }
+            }
+        }
+    }
+}
