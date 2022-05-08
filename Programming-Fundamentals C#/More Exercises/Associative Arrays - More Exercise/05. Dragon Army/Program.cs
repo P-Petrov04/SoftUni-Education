e8e8e9d8128e9778d3816c7df9c695689866367e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-08 20:59:44 +0300

    Add solution of "Dragon Army"

---
+++ Programming-Fundamentals C#/More Exercises/Associative Arrays - More Exercise/05. Dragon Army/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Dragon_Army
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
