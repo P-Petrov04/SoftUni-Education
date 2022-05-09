9a59cd79ae5dcb3c1f217421ecb667ecf595f5bb
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-09 22:18:52 +0300

    Add solution of "Reverse String" and .sln file

---
+++ Programming-Fundamentals C#/15.Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                List<char> wordChars = new List<char>();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char currentChar = input[i];
+                    wordChars.Add(currentChar);
+                }
+
+                wordChars.Reverse();
+                string reversedWord = string.Empty;
+                foreach (char ch in wordChars)
+                {
+                    reversedWord += ch;
+                }
+
+                //for (int i = input.Length - 1; i >= 0; i--)
+                //{
+                //    char currentChar = input[i];
+                //    reversedWord += currentChar;
+                //}
+
+                Console.WriteLine($"{input} = {reversedWord}");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
