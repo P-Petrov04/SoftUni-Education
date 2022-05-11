4535d80b04d78e047511a399477de32924fe2199
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-11 22:56:10 +0300

    Add solution of "Repeat String"

---
+++ Programming-Fundamentals C#/15.Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string repeatedWords = string.Empty;
+            //StringBuilder result = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    repeatedWords += word;
+                    //result.Append(word);
+                }
+            }
+
+            Console.WriteLine(repeatedWords);
+            //Console.WriteLine(result);
+        }
+    }
+}
