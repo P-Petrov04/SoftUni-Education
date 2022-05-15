4c25dd593b4fcfb589dc53a8c44f4aa4c64a2b7b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-15 21:40:21 +0300

    Add solution of "Substring"

---
+++ Programming-Fundamentals C#/15.Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string word = Console.ReadLine();
+
+            while (word.Contains(wordToRemove))
+            {
+                int indexOfWordToRemove = word.IndexOf(wordToRemove);
+                word = word.Remove(indexOfWordToRemove, wordToRemove.Length);
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
