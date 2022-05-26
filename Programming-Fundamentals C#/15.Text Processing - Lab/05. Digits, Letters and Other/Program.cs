27ec3ccfddd6c6066f2ccd071afb0010bb6241fa
P-Petrov04 <petar.004.petrov@gmail.com>
2022-05-26 15:11:55 +0300

    Add solutions of 2 exercises - "Text Filter" and "Digits, Letters and Other"

---
+++ Programming-Fundamentals C#/15.Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbols = Console.ReadLine();
+
+            string letters = string.Empty;
+            string digits = string.Empty;
+            string otherSymbols = string.Empty;
+
+            foreach (char ch in symbols)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digits += ch;
+                }
+                else if (char.IsLetter(ch))
+                {
+                    letters += ch;
+                }
+                else if (!char.IsLetterOrDigit(ch))
+                {
+                    otherSymbols += ch;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(otherSymbols);
+        }
+    }
+}
