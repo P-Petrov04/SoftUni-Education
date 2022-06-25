b3d680af363b9b8f5ed70791c2a1ec9cb35b030b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-25 19:20:49 +0300

    Add solution of "Replace Repeating Chars"

---
+++ Programming-Fundamentals C#/16.Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currCh = text[i];
+                int count = 0;
+                for (int y = i + 1; y < text.Length; y++)
+                {
+                    if (text[y] == currCh)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                text = text.Remove(i, count);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
