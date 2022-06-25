ccc6159093caa90ba9bfc7cf52b7a0e6063469ca
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-25 19:21:09 +0300

    Add solution of "String Explosion"

---
+++ Programming-Fundamentals C#/16.Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            List<char> newString = new List<char>();
+            int explosionPower = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentCh = text[i];
+                if (currentCh == '>')
+                {
+                    newString.Add(currentCh);
+                    double powerOfExplosion = char.GetNumericValue(text[i + 1]);
+                    explosionPower += (int)powerOfExplosion;
+                }
+                else
+                {
+                    if (explosionPower == 0)
+                    {
+                        newString.Add(currentCh);
+                    }
+                    else
+                    {
+                        explosionPower--;
+                    }
+                }
+            }
+
+            string finalString = string.Join("", newString);
+            Console.WriteLine(finalString);
+        }
+    }
+}
