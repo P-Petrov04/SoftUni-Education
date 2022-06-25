ea983ee801d89c963842979d532e3f7316c5220e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-25 19:19:10 +0300

    Add solution of "Extract File"

---
+++ Programming-Fundamentals C#/16.Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToAFile = Console.ReadLine();
+
+            int startingIndexOfTheFileName = pathToAFile.LastIndexOf('\\');
+            int endingIndexOfTheFileName = pathToAFile.IndexOf('.');
+
+            string fileName = pathToAFile.Substring(startingIndexOfTheFileName + 1, endingIndexOfTheFileName -
+                startingIndexOfTheFileName - 1);
+            string extension = pathToAFile.Substring(endingIndexOfTheFileName + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+
+        }
+    }
+}
