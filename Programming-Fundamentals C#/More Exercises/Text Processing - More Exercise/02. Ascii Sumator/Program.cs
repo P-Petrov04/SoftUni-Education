7a9d7476319afb18c54ff87eb31e01384f709820
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-26 23:51:17 +0300

    Add solution of "Ascii Sumator"

---
+++ Programming-Fundamentals C#/More Exercises/Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCh = char.Parse(Console.ReadLine());
+            char secondCh = char.Parse(Console.ReadLine());
+            string randomStr = Console.ReadLine();
+
+            int sum = 0;
+            List<char> validCh = randomStr.Where(ch => (int)ch > firstCh && (int)ch < secondCh).ToList();
+            foreach (char ch in validCh)
+            {
+                sum += (int)ch;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
