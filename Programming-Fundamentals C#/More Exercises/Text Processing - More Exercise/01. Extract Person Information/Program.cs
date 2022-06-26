8532385a85650682d411d8216bcdd64ca32a9a6e
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-26 23:50:38 +0300

    Add solution of "Extract Person Information"

---
+++ Programming-Fundamentals C#/More Exercises/Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternName = @"\@(?<name>[A-Za-z]+)\|";
+            string patternAge = @"\#(?<age>[0-9]+)\*";
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string text = Console.ReadLine();
+                Match matchName = Regex.Match(text, patternName);
+                Match matchAge = Regex.Match(text, patternAge);
+
+                Console.WriteLine($"{matchName.Groups["name"].Value} is {matchAge.Groups["age"].Value} years old.");
+            }
+        }
+    }
+}
