db1735331f421b7b436767f0fba4705d1b90a437
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-28 17:52:45 +0300

    Add solution of "Match Phone Number"

---
+++ Programming-Fundamentals C#/17.Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([ |-])2\1\d{3}\1\d{4}\b";
+
+            string phones = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(phones, pattern);
+            List<string> listOfNumbers = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                listOfNumbers.Add(match.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", listOfNumbers));
+        }
+    }
+}
