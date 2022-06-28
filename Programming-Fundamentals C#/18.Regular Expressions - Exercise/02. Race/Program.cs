46529ae1a09c0cd96435fd38e80632af10ea2326
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-28 18:00:20 +0300

    Add solution of "Race"

---
+++ Programming-Fundamentals C#/18.Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, int> runnersArg = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+            while (input != "end of race")
+            {
+                StringBuilder nameFromInput = new StringBuilder();
+                int distance = 0;
+                foreach (char ch in input)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        nameFromInput.Append(ch);
+                    }
+                    else if (char.IsDigit(ch))
+                    {
+                        distance += (int)char.GetNumericValue(ch);
+                    }
+                }
+                string name = nameFromInput.ToString();
+                if (names.Contains(name))
+                {
+                    if (runnersArg.Keys.Contains(name))
+                    {
+                        runnersArg[name] += distance;
+                    }
+                    else
+                    {
+                        runnersArg.Add(name, distance);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var result = runnersArg.OrderByDescending(x => x.Value);
+            int counter = 1;
+            foreach (var runner in result)
+            {
+                if (counter == 1)
+                {
+                    Console.WriteLine($"1st place: {runner.Key}");
+                }
+                else if (counter == 2)
+                {
+                    Console.WriteLine($"2nd place: {runner.Key}");
+                }
+                else if (counter == 3)
+                {
+                    Console.WriteLine($"3rd place: {runner.Key}");
+                }
+                else
+                {
+                    break;
+                }
+                counter++;
+            }
+        }
+    }
+}
