e77a6f6be6d87749a2a76a462764ed4e4c48c955
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-28 17:53:08 +0300

    Add solution of "Match Dates"

---
+++ Programming-Fundamentals C#/17.Regular Expressions/03. Match Dates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\d{2})([\.|\/|-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            string dates = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(dates, pattern);
+            foreach (Match match in matches)
+            {
+                string day = match.Groups["day"].Value;
+                string month = match.Groups["month"].Value;
+                string year = match.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+                //Day: 13, Month: Jul, Year: 1928
+            }
+        }
+    }
+}
