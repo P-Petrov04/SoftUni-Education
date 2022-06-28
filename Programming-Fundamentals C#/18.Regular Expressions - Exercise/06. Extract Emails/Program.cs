802407c33d0ed0ef9c019aa8a2ca8cfed01f0f94
P-Petrov04 <petar.004.petrov@gmail.com>
2022-06-28 18:01:59 +0300

    Add solution of "Extract Emails"

---
+++ Programming-Fundamentals C#/18.Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
