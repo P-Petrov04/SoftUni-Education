3d5c59834342c6be7dc9027555fac0191a12453b
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-01 15:33:08 +0300

    Add solution of "Santa's Secret Helper"

---
+++ Programming-Fundamentals C#/More Exercises/Regular Expressions - More Exercise/4. Santa's Secret Helper/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _4._Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\@(?<name>[A-Za-z]+)([^\@|\-|\!|\:|\>]*?)\!(?<type>[G|N]{1})\!";
+            List<string> goodChildren = new List<string>();
+            int key = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int count = 0;
+            while (input != "end")
+            {
+                StringBuilder decriptedMessage = new StringBuilder();
+                foreach (char ch in input)
+                {
+                    char newCh = (char)(ch - key);
+                    decriptedMessage.Append(newCh);
+                }
+                string message = decriptedMessage.ToString();
+                Match match = Regex.Match(message, pattern);
+                if (match.Success)
+                {
+                    if (match.Groups["type"].Value == "G")
+                    {
+                        goodChildren.Add(match.Groups["name"].Value);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (goodChildren.Count != 0)
+            {
+                foreach (string child in goodChildren)
+                {
+                    Console.WriteLine(child);
+                }
+            }
+        }
+    }
+}
