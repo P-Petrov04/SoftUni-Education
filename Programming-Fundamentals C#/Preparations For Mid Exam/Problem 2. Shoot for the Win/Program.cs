5bc1851536ae1c3a31acc11e19261b5440311e0d
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-02 21:38:25 +0300

    Add solution of "Shoot for the Win"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 2. Shoot for the Win/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_2._Shoot_for_the_Win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+            int countOfShoots = 0;
+            while (input != "End")
+            {
+                int indexToShoot = int.Parse(input);
+                if (indexToShoot < 0 || indexToShoot >= numbers.Count)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                if (numbers[indexToShoot] != -1)
+                {
+                    countOfShoots++;
+                    int currentNum = numbers[indexToShoot];
+                    numbers[indexToShoot] = -1;
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] > currentNum && numbers[i] != -1)
+                        {
+                            numbers[i] -= currentNum;
+                        }
+                        else if (numbers[i] <= currentNum && numbers[i] != -1)
+                        {
+                            numbers[i] += currentNum;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Shot targets: {countOfShoots} -> {string.Join(" ", numbers)}");
+        }
+    }
+}
