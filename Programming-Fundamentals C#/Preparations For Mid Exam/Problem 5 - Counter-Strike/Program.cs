192a0d3ed46e2a83c2a8a23942570bbe5d921d43
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-05 00:28:53 +0300

    Add solution of "Counter-Strike"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 5 - Counter-Strike/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Problem_5___Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myEnergy = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int countOfWins = 0;
+            int countOfWinsInRow = 0;
+
+            while (input != "End of battle" && myEnergy > 0)
+            {
+                int distanceToEnemy = int.Parse(input);
+                if (myEnergy < distanceToEnemy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {countOfWins} won battles and {myEnergy} energy");
+                    return;
+                }
+                myEnergy -= distanceToEnemy;
+                countOfWins++;
+                countOfWinsInRow++;
+                if (countOfWinsInRow == 3)
+                {
+                    myEnergy += countOfWins;
+                    countOfWinsInRow = 0;
+                }
+
+                input = Console.ReadLine();
+            }
+            if (input == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {countOfWins}. Energy left: {myEnergy}");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough energy! Game ends with {countOfWins} won battles and {myEnergy} energy");
+            }
+        }
+    }
+}
