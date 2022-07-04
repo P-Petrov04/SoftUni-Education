8048abb977d4844ed216e8c65a338cd7de69e814
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-05 00:28:28 +0300

    Add solution of "The Lift"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 4 - The Lift/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_4___The_Lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int capacityOfOneWagon = 4;
+            int peopleWaiting = int.Parse(Console.ReadLine());
+            List<int> peopleInWagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int fullWagons = 0;
+
+            for (int i = 0; i < peopleInWagons.Count; i++)
+            {
+                if (peopleWaiting == 0)
+                {
+                    break;
+                }
+                while (peopleInWagons[i] < 4 && peopleWaiting > 0)
+                {
+                    peopleInWagons[i]++;
+                    peopleWaiting--;
+                }
+                if (peopleInWagons[i] == 4)
+                {
+                fullWagons++;
+                }
+            }
+
+            if (peopleWaiting == 0 && fullWagons == peopleInWagons.Count)
+            {
+                Console.WriteLine(string.Join(" ", peopleInWagons));
+            }
+            else if (peopleWaiting == 0)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", peopleInWagons));
+            }
+            else if (peopleWaiting > 0 && fullWagons == peopleInWagons.Count)
+            {
+                Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+                Console.WriteLine(string.Join(" ", peopleInWagons));
+            }
+        }
+    }
+}
