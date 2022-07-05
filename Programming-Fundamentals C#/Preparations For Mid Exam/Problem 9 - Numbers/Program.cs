ea329e0318b2b8a1dc39dac06c767085e740c18a
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-06 01:57:51 +0300

    Add solution of "Numbers"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 9 - Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_9___Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int sum = numbers.Sum();
+            double averageValue = (double)sum / numbers.Count;
+            numbers.RemoveAll(number => number <= averageValue);
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            numbers.Sort((a, b) => b.CompareTo(a));
+
+            if (numbers.Count > 5)
+            {
+                while (numbers.Count > 5)
+                {
+                    numbers.RemoveAt(numbers.Count - 1);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
