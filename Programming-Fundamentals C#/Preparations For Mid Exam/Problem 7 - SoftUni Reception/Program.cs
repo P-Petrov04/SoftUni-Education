e96181d7c13112e095e7fb1a259d693e7b92f48f
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-06 01:57:05 +0300

    Add solution of "SoftUni Reception"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 7 - SoftUni Reception/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem_7___SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employee1 = int.Parse(Console.ReadLine());
+            int employee2 = int.Parse(Console.ReadLine());
+            int employee3 = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int allEmployeesPerHour = employee1 + employee2 + employee3;
+            int allHours = 0;
+            int hoursInRow = 0;
+            int breakHours = 0;
+            while (studentsCount > 0)
+            {
+                if (hoursInRow == 3)
+                {
+                    breakHours++;
+                    hoursInRow = 0;
+                    continue;
+                }
+                studentsCount -= allEmployeesPerHour;
+                allHours++;
+                hoursInRow++;
+            }
+
+            Console.WriteLine($"Time needed: {allHours + breakHours}h.");
+        }
+    }
+}
