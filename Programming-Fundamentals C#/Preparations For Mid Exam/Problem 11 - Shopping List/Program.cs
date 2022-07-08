c5ac260abf77ee8cf379667b8013cee984a40552
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-08 14:15:36 +0300

    Add solution of "Shopping List"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 11 - Shopping List/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_11___Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine().Split("!").ToList();
+            string input = Console.ReadLine();
+
+            while (input != "Go Shopping!")
+            {
+                string[] commands = input.Split();
+                string typeCommand = commands[0];
+
+                if (typeCommand == "Urgent")
+                {
+                    string item = commands[1];
+                    if (groceries.Contains(item))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        groceries.Insert(0, item);
+                    }
+                }
+                else if (typeCommand == "Unnecessary")
+                {
+                    string item = commands[1];
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+                    else
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else if (typeCommand == "Correct")
+                {
+                    string oldItem = commands[1];
+                    string newItem = commands[2];
+                    if (groceries.Contains(oldItem))
+                    {
+                        int indexOfOldItem = -1;
+                        for (int i = 0; i < groceries.Count; i++)
+                        {
+                            if (groceries[i] == oldItem)
+                            {
+                                indexOfOldItem = i;
+                            }
+                        }
+                        groceries.Remove(oldItem);
+                        groceries.Insert(indexOfOldItem, newItem);
+                    }
+                    else
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else if (typeCommand == "Rearrange")
+                {
+                    string item = commands[1];
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                        groceries.Add(item);
+                    }
+                    else
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }        
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", groceries));
+        }
+    }
+}
