0d84f2f3307019faef27945c63857c57860a70d1
P-Petrov04 <petar.004.petrov@gmail.com>
2022-07-14 20:43:10 +0300

    Add solution of "Bonus Scoring System"

---
+++ Programming-Fundamentals C#/Preparations For Mid Exam/Problem 13 - Bonus Scoring System/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem_13___Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+            int countOfLectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+            
+            double maxBonus = 0;
+            int maxLectures = 0;
+
+            for (int i = 1; i <= countOfStudents; i++)
+            {
+                int attendanceOfEachStudent = int.Parse(Console.ReadLine());
+
+                double bonus = (double)attendanceOfEachStudent / countOfLectures * (5 + additionalBonus);
+                if (bonus > maxBonus)
+                {
+                    maxBonus = bonus;
+                    maxLectures = attendanceOfEachStudent;
+                }
+            }
+
+            maxBonus = Math.Ceiling(maxBonus);
+            Console.WriteLine($"Max Bonus: {maxBonus}.");
+            Console.WriteLine($"The student has attended {maxLectures} lectures.");
+        }
+    }
+}
