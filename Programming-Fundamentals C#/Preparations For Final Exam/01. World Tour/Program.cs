1af759ffe1e5b8295d0f2c379b40c91b4e4090e2
PetarPetrov04 <petar.004.petrov@gmail.com>
2022-12-01 23:52:25 +0200

    Add solution of "World Tour"

---
+++ Programming-Fundamentals C#/Preparations For Final Exam/01. World Tour/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _01._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string command = Console.ReadLine();
+            while (command != "Travel")
+            {
+                string[] commandArgs = command.Split(":");
+
+                string typeCommand = commandArgs[0];
+                if (typeCommand == "Add Stop")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    string newStop = commandArgs[2];
+                    if (IsIndexValid(index, stops))
+                    {
+                        stops = stops.Insert(index, newStop);
+                    }
+                    Console.WriteLine(stops);
+                }
+                else if (typeCommand == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+                    if (IsIndexValid(startIndex, stops) && IsIndexValid(endIndex, stops))
+                    {
+                        stops = stops.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+                    Console.WriteLine(stops);
+                }
+                else if (typeCommand == "Switch")
+                {
+                    string oldStr = commandArgs[1];
+                    string newStr = commandArgs[2];
+                    if (stops.Contains(oldStr))
+                    {
+                        stops = stops.Replace(oldStr, newStr);
+                    }
+                    Console.WriteLine(stops);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+
+        static bool IsIndexValid(int index, string stops)
+        {
+            if(index < 0 || index >= stops.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
